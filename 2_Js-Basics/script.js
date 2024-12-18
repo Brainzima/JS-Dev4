932b8fdb6b9b4718968c17294949392ba9616eae
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-18 17:12:37 +0530

    Add files via upload

---
+++ 2_Js-Basics/script.js
@@ -0,0 +1,18 @@
+// Take two numbers and display its sum , subtraction, multipliocation, division
+
+num1 = 100
+num2 = 50
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+// * asterisk
+console.log(num1)
+console.log(num2)
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+
+document.write("Helloooooo Aliens")
+document.write(div)
