3fa4c242198839876ef57deb6c51823bced2b4c2
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-20 11:20:34 +0000

    user input and literals

---
+++ 3_JS-Basics/script.js
@@ -0,0 +1,11 @@
+// take the radius from the user and display area of circle 
+
+pie = 3.14
+radius = prompt("Enter the radius")
+area = pie * radius * radius
+// console.log(area)
+
+// document.write("area of circle is ")
+// document.write(area)
+
+document.write(`Value of Pie = ${pie} , Entered Radius is = ${radius} , Area of Circle is = ${area}`)
