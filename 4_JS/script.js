85db3dc24b8429087b34126eb26c663f8a6184c7
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-23 11:35:41 +0000

    rectangle and popups

---
+++ 4_JS/script.js
@@ -0,0 +1,18 @@
+l = prompt("Enter the Lenth of Rectanger:")
+w = prompt("Enter the Width of Rectanger:")
+
+area = l * w
+perimeter = 2 * ( l + w )
+
+document.write(`Value of Length ${l} , Value of Width ${w} , Area of Rectangle ${area} , Perimeter is ${perimeter}`)
+
+
+
+
+// understanding
+// console.log(546435545) //numbers
+// console.log(435435fdhgfdh) // wrong method
+// name = 'Brainzima'
+// console.log("dfsetr4tedrtgt45terdty4356@##$%^^$^$36 ${name}")
+// console.log('dfsetr4tedrtgt45terdty4356@##$%^^$^$36 ${name}')
+// console.log(`dfsetr4tedrtgt45terdty4356@##$%^^$^$36 ${name}`)
