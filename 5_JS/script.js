891b652fafd3069fdff59e10b10a210a38fc2fa2
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-24 11:53:26 +0000

    if statements

---
+++ 5_JS/script.js
@@ -0,0 +1,17 @@
+// if else statement
+// if (condition){
+    // if true then statement
+// }else{
+    // if false then statement
+// }
+
+// take age from user and display either he/she is eligible for vote or not
+age = prompt("Enter your age:")
+
+if ( age >= 18 ){
+    alert("Congratulations You are bara aadmi!")
+}else{
+    alert("Oops! You are chhotuuu!")
+}
+
+
