4f150f115d889f27b34638c76d0358e963d340c7
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-01-06 12:02:00 +0000

    class list and addclass removeclass

--- 8_JS/script.js
+++ 8_JS/script.js
@@ -11,26 +11,32 @@ function classFunc(){
 function red(){
     document.querySelector('.name').style="color:red;transition:.6s;"
     document.querySelector('.image').src="https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg"
+    document.querySelector('.image').style="display:block;"
 }
 function blue(){
     document.querySelector('.name').style="color:blue;transition:.6s;"
     document.querySelector('.image').src="https://themeisle.com/blog/wp-content/uploads/2024/06/Online-Image-Optimizer-Test-Image-JPG-Version.jpeg"
+    document.querySelector('.image').style="display:block;"
 }
 function yellow(){
     document.querySelector('.name').style="color:orange;transition:.6s;"
     document.querySelector('.image').src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"
+    document.querySelector('.image').style="display:block;"
 }
 function pink(){
     document.querySelector('.name').style="color:#fc03f4;transition:.6s;"
     document.querySelector('.image').src="https://cdn.pixabay.com/photo/2023/03/15/20/46/background-7855413_640.jpg"
+    document.querySelector('.image').style="display:block;"
 }
 function aqua(){
     document.querySelector('.name').style="color:aqua;transition:.6s;"
     document.querySelector('.image').src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+    document.querySelector('.image').style="display:block;"
 }
 function purple(){
     document.querySelector('.name').style="color:purple;transition:.6s;"
     document.querySelector('.image').src="https://th.bing.com/th/id/OIG4.hb6x6NsHBTvbDGaD13Ua"
+    document.querySelector('.image').style="display:block;"
 }
 
 
