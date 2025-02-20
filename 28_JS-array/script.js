1eca98a8d107d121e0ea38c6e0103775ddfc6cee
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-02-20 10:47:00 +0000

    array

---
+++ 28_JS-array/script.js
@@ -0,0 +1,14 @@
+// const nam = "Rakesh"
+
+// console.log(nam)
+
+const room = ['Rakesh','Naresh','Suresh','Mukesh','Jitesh']
+
+console.log(room)
+
+// display single data
+// console.log(room[0])
+
+
+str = `Hello ${room[2]}, How are you?`
+console.log(str)
