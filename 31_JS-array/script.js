70b3f0b4ba97f6d277fcc27be64235e142f540ef
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-02-25 11:43:20 +0000

    array methods in js

---
+++ 31_JS-array/script.js
@@ -0,0 +1,37 @@
+// let a = 'Rakesh'
+// a = 'Mukesh'
+
+// console.log(a)
+
+// Array
+let names = ['Rakesh','Mukesh','Suresh','Naresh',79797979,415.14,'Khuriyal','brainzima']
+
+console.log(names)
+
+// single data access
+console.log(names[5])
+
+// to know the length of array
+console.log(names.length)
+
+// remove last data from Array
+names.pop()
+
+console.log(names)
+
+// remove a data from Array
+names.splice(3,2)
+
+console.log(names)
+
+// add a data in Array
+names.push('Ramesh')
+console.log(names)
+names.push('Kashmir')
+// sorting the array data
+names.sort()
+console.log(names)
+
+// reverse the array data
+names.reverse()
+console.log(names)
